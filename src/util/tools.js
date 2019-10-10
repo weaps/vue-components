64a9667bbe667e7f7ce48fad6c379606e891f894
weaps <weaps@qq.com>
2019-10-10 23:27:50 +0800

    add deepclone

--- src/util/tools.js
+++ src/util/tools.js
@@ -8,6 +8,22 @@ const util =  {
   },
   getDate(year, month=0, day=1) {
     return new Date(year, month, day)
+  },
+  deepclone(obj) {
+    // debugger
+    if (obj == null) return obj
+    if (typeof obj != 'object') return obj
+    // 判断是否是日期对象
+    if (obj instanceof Date) return new Date(obj)
+    // 判断是否是正则对象
+    if (obj instanceof RegExp) return new RegExp(obj)
+    let res = new obj.constructor
+    for(let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        res[key] = this.deepclone(obj[key])
+      }
+    }
+    return res
   }
 }
 export default util
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,58 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button>asdfasd</el-button>
+    <el-checkbox-group v-model="checkedCities" @change="a => handleCheckedCitiesChange('a',...a)">
+      <el-checkbox v-for="city in cityOptions" :label="city" :key="city">{{city}}</el-checkbox>
+    </el-checkbox-group>
+    <input type="file" @change="changeFile">
   </div>
 </template>
+<script>
+  import util from "../util/tools";
+
+  export default {
+    data() {
+      return {
+        checkAll: false,
+        cityOptions: ['上海', '北京', '广州', '深圳'],
+        checkedCities: ['上海', '北京'],
+        cities: this.cityOptions,
+        isIndeterminate: true
+      }
+    },
+    mounted() {
+      let arr = {a:{arr: [1,2,3,{sex: 'male'}]}}
+      let res = util.deepclone(arr)
+      arr.b = 'test'
+      console.log(arr,res)
+      let instance = {
+        [Symbol.hasInstance] (value) {
+          return true
+        }
+      }
+      console.log({a:1} instanceof instance)
+    },
+    methods: {
+      handleCheckedCitiesChange(...a) {
+        console.log(a)
+      },
+      changeFile(e) {
+        debugger
+        let reader = new FileReader()
+        reader.onload = function () {
+          debugger
+          console.log(reader)
+        }
+      }
+    }
+  }
+</script>
+<style lang="sass" scoped>
+  .el-button
+    border-color: #2c3e50
+  .el-switch
+    &.is-checked
+      .el-switch__core
+        background: aqua
+</style>
