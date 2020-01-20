dbeb270d3e70673fd9179f7a3f4b720d83e52139
weaps <weaps@qq.com>
2020-01-20 17:20:47 +0800

    update

---
+++ src/views/dom.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="dom">dom
+  </ul>
+</template>
+<script>
+export default {
+  name: 'dom',
+  data() {
+    return {
+      total: 100000
+    }
+  },
+  mounted() {
+    // debugger
+    let dom = document.querySelector('.dom')
+    for(let i = 0; i < this.total; i++) {
+      let li = document.createElement('li')
+      li.innerText = i
+      dom.appendChild(li)
+    }
+  }
+}
+</script>
